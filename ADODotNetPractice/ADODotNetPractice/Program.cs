9922c2b22892a54e83d7e0b4698d9a81e9d52933
67418_Rutuja <rutubkakad@gmail.com>
2025-01-14 07:45:40 +0530

    Peroformd the CRUD operations

---
+++ ADODotNetPractice/ADODotNetPractice/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace ADODotNetPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddInput();
+            UserRepo repo = new UserRepo();
+            repo.GetAllUsers();
+
+            var updatedUser = new UserDetails
+            {
+                UserId = 1,
+                FirstName = "Johnny",
+                LastName = "Daren",
+                Email = "johnny.doe@example.com",
+                Password = "newpassword123"
+            };
+            repo.UpdateUser(updatedUser);
+            repo.DeleteUser(11);
+        }
+        public static void AddInput()
+        {
+            UserRepo repo = new UserRepo();
+            UserDetails user = new UserDetails();
+            user.FirstName = "John";
+            user.LastName = "Dsouza";
+            user.Email = "john@gmail.com";
+            user.Password = "John@123";
+            Console.WriteLine(repo.AddUserDetails(user) ? "Data added successfully": "Operation failed");
+        }
+
+        
+    }
+
+}
---
+++ ADODotNetPractice/ADODotNetPractice/UserRepo.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace ADODotNetPractice
+{
+    public class UserRepo
+    {
+        public static string connectionString = @"Data Source=LAPTOP-HB42IHJ0\SQLEXPRESS;Initial Catalog=DemoDBPractice;Integrated Security=True;TrustServerCertificate=True";
+        public string connectionConnection = @"Server=LAPTOP-HB42IHJ0\SQLEXPRESS;Database=DemoDBPractice;Trusted_Connection=True";
+
+        SqlConnection connection = new SqlConnection(connectionString);// throwing exception
+
+        public bool AddUserDetails(UserDetails user)
+        {
+            using (connection)
+            {
+                SqlCommand cmd = new SqlCommand("spAddUserDetails", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FirstName",  user.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", user.LastName);
+                cmd.Parameters.AddWithValue("@Email", user.Email);
+                cmd.Parameters.AddWithValue("@Password", user.Password);
+                connection.Open();
+                var result = cmd.ExecuteNonQuery();
+                if (result != 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public void GetAllUsers()
+        {
+            UserDetails users = new UserDetails();
+            using (connection)
+            {
+                string query = "select * from UserDetails";
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        users.UserId = reader.GetInt32(0);
+                        users.FirstName = reader.GetString(1);
+                        users.LastName = reader.GetString(2);
+                        users.Email = reader.GetString(3);
+                        users.Password = reader.GetString(4);
+                        Console.WriteLine("{0}, {1}, {2}, {3}, {4}", users.UserId, users.FirstName, users.LastName, users.Email, users.Password);
+
+                    }
+                }
+            }
+        }
+
+
+        public void UpdateUser(UserDetails user)
+        {
+            using (connection)
+            {
+                SqlCommand cmd = new SqlCommand("spUpdateUser", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UserId", user.UserId);
+                cmd.Parameters.AddWithValue("@FirstName", user.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", user.LastName);
+                cmd.Parameters.AddWithValue("@Email", user.Email);
+                cmd.Parameters.AddWithValue("@Password", user.Password);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("User updated successfully!");
+
+            }
+        }
+
+        public void DeleteUser(int userId)
+        {
+            using (connection)
+            {
+                SqlCommand cmd = new SqlCommand("spDeleteUser", connection);
+                cmd.CommandType= CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UserId", userId);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("User deleted successfully!");
+            }
+        }
+    }
+}
